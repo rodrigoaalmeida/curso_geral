9bb090fff3359dec13c5b24d5684d2f36710f508
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2022-02-01 17:29:19 -0400

    Aula de vetor, altura media

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -11,37 +11,21 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter account number: ");
-		int accountNumber = sc.nextInt();
-		System.out.print("Enter account holder: ");
-		sc.nextLine();
-		String accountHolder = sc.nextLine();
-		System.out.print("Is there na initial deposit (y/n)? ");
-		String initialDeposit = sc.next().substring(0);
-
-		double initialDepositValue;
-		if (initialDeposit.equalsIgnoreCase("y")) {
-			System.out.print("Enter initial deposit value: ");
-			initialDepositValue = sc.nextDouble();
-		} else {
-			initialDepositValue = 0;
+		int n = sc.nextInt();
+		double[] vect = new double[n];
+		
+		for(int i=0; i < n; i++) {
+			vect[i] = sc.nextDouble();
 		}
 		
-		Conta account = new Conta(accountNumber, accountHolder, initialDepositValue);
-
-		System.out.println();
-		System.out.println("Account data:");
-		System.out.println(account);
+		double sum = 0.0;
+		for(int i = 0; i < n; i++) {
+			sum += vect[i];
+		}
 		
-		System.out.println();
-		System.out.print("Enter a deposit value: ");
-		account.deposit(sc.nextDouble());
-		System.out.println(account);
+		double avg = sum / n;
 		
-		System.out.println();
-		System.out.print("Enter a withdraw value: ");
-		account.withdraw(sc.nextDouble());
-		System.out.println(account);
+		System.out.printf("Average height: %.2f%n", avg);
 
 		sc.close();
 	}
