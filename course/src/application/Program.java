0fc32e43602dc8078a2d8e59235c3ec8785f8ba3
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2022-02-03 16:32:21 -0400

    Aula de List

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,51 +1,51 @@
 package application;
 
-import java.util.Locale;
-import java.util.Scanner;
-
-import entities.Room;
-
-
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-
-		Room[] vectRooms = new Room[10];
 		
-		System.out.print("How many rooms will be rented? ");
-		int qtdInquilinos = sc.nextInt();
-		System.out.println();
+		List<String> list = new ArrayList<>();
 		
-		for(int i = 0; i < qtdInquilinos; i++) {
-			int cont = i + 1;
-			System.out.println("Rent #" + cont);
-			System.out.print("Name: ");
-			sc.nextLine();
-			String name = sc.nextLine();
-			System.out.print("Email: ");
-			String email = sc.nextLine();
-			System.out.print("Room: ");
-			int numeroQuarto = sc.nextInt();
-			System.out.println();
-			
-			vectRooms[numeroQuarto] = new Room(name, email);
+		list.add("Maria");
+		list.add("Alex");
+		list.add("Bob");
+		list.add("Anna");
+		list.add(2, "Marco");
+		
+		System.out.println(list.size());
+		
+		for(String x : list) {
+			System.out.println(x);
 		}
 		
-		System.out.println();
-		System.out.println("Busy rooms:");
+		System.out.println("-------------------------------------");
 		
-		for(int i = 0; i < vectRooms.length; i++) {
-			if( vectRooms[i] != null) {
-				System.out.println(i + ": " + vectRooms[i].getName() + ", "
-						+ vectRooms[i].getEmail());
-			}
+		list.removeIf(x -> x.charAt(0) == 'M');
+		for(String x : list) {
+			System.out.println(x);
 		}
-
-		sc.close();
+		
+		System.out.println("--------------------------------------");
+		
+		System.out.println("Index of Bob: " + list.indexOf("Bob"));
+		System.out.println("Index of Marco: " + list.indexOf("Marco"));
+		
+		System.out.println("---------------------------------------");
+		
+		List<String> result = list.stream().filter(x -> x.charAt(0) == 'A')
+				.collect(Collectors.toList());
+		for(String x : result) {
+			System.out.println(x);
+		}
+		
+		System.out.println("---------------------------------------");
+		String name = list.stream().filter(x -> x.charAt(0) == 'J')
+				.findFirst().orElse(null);
+		System.out.println(name);
 	}
 
 }
